cbe3f636540a11d67beb061c8663c7970654d189
alex <a.gilmore1993@gmail.com>
2025-01-06 09:19:37 -0600

    refactor heatmap into 1 function call, add example

--- tests/unit/visualize/test_heatmap.py
+++ tests/unit/visualize/test_heatmap.py
@@ -1,10 +1,14 @@
 from pyspark.sql import DataFrame
 
-from neo4j_parallel_spark_loader.visualize.heatmap import create_heatmap_dataframe
+from neo4j_parallel_spark_loader.visualize.heatmap import (
+    _format_spark_dataframe_for_visualization,
+)
 
 
 def test_create_heatmap_dataframe(heatmap_spark_dataframe: DataFrame) -> None:
-    df = create_heatmap_dataframe(spark_dataframe=heatmap_spark_dataframe)
+    data = _format_spark_dataframe_for_visualization(
+        spark_dataframe=heatmap_spark_dataframe
+    )
 
-    assert heatmap_spark_dataframe.count() == df.shape[0]
-    assert set(heatmap_spark_dataframe.columns) == set(df.columns)
+    assert heatmap_spark_dataframe.count() == len(data)
+    assert set(heatmap_spark_dataframe.columns) == set(data[0].keys())
